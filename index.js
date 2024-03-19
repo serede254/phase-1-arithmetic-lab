4c398f7ce476ab768d727fd684b177ba44b41d3c
root <root@SURFACE.>
2024-03-19 11:53:16 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+
+var multiply;
+let num1=31,num2=2;
+multiply =num1*num2;
+
+var random;
+random =Math.floor(Math.random())+1;
+
+var mod;
+let num3=36,num4=8;
+mod=num3%num4;
+
+var max;
+max= Math.max(1,2,4,5,6,13,16,20);
